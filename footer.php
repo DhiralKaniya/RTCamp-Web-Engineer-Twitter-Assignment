3c7cd43864a330e6adf804ac53c103a5adbe4976
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-08-06 14:13:13 +0530

    Bug Solved

--- footer.php
+++ footer.php
@@ -5,6 +5,7 @@
          </div>
         <div class="navbar-text pull-right"> 
         	<a href="https://www.facebook.com/dk.Boass" target="_new"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;
+            <a href="https://github.com/DhiralKaniya" target="_new"><i class="fa fa-github fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;
         	<a href="https://twitter.com/dhiralskaniya" target="_new"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;  
         	<a href="https://www.linkedin.com/in/dhiral-kaniya-3b232290" target="_new"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></i></a>&nbsp;&nbsp; 
         	<a href="https://www.instagram.com/imdhiral/" target="_new"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></i></a>&nbsp;&nbsp;  
