b5a08c26a4f35e8984369e6809983593409f8320
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-08-06 13:42:01 +0530

    Configuration file updated

---
+++ config.php
@@ -0,0 +1,17 @@
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+	<link rel="shortcut icon" href="image/logo.jpg">
+	<link href="lib\css\bootstrap.min.css" rel="stylesheet">
+
+	<script type="text/javascript" src="lib\js\bootstrap.min.js"></script>
+	
+	<link href="lib\css\custome.css"  rel="stylesheet" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet">
+	
