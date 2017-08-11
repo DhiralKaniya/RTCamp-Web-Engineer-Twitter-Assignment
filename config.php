9fe222e87515288d27706019cd1dcd6d9222fe5d
DhiralKaniya <dhiralkaniya3146@gmail.com>
2017-08-11 23:43:47 +0530

    UI Update

--- config.php
+++ config.php
@@ -19,4 +19,4 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 	<link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet">
-	
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
